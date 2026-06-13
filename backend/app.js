6d8e13877a1263677e1f86866a64bed824c8efb0
Shrivas-shetty <shrivasshetty2006@gmail.com>
2026-06-13 20:00:45 +0530

    added app.use(cors) with a dummy frontend link

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,12 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
-app.use(cors());
+
+
+app.use(cors({
+  origin: "https://your-frontend.vercel.app",
+  credentials: true
+}));
 
 // Middleware
 app.use(express.json());
